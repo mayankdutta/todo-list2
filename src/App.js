d312f0f430ef7a6a87911b8b524bcec4c1e148e5
Mayank Sharma <info@Mayanks-MacBook-Air.local>
2021-05-12 11:25:00 +0530

    tailwind attached

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-green-400">
+      <div className=" my-80 bg-gray-400 grid grid-cols-1 justify-items-center justify-center content-center items-center">
+        hii
+        <div className=""> </div>
+      </div>
     </div>
   );
 }
